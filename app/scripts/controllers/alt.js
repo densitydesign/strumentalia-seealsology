b3e4539151a1cf80c11b01efda1fd3edb35445b8
Daniele Ciminieri <stonecoldcrazy@gmail.com>
2014-11-14 18:04:16 +0100

    new layout

--- app/scripts/controllers/alt.js
+++ app/scripts/controllers/alt.js
@@ -1,7 +1,16 @@
 'use strict';
 
 angular.module('wikiDiverApp')
-    .controller('AltCtrl', function ($scope, $http, $q) {
+    .controller('AltCtrl', function ($scope, $http) {
+
+
+        $('#tokenfield').tokenfield({
+            autocomplete: {
+                source: ['red', 'blue', 'green', 'yellow', 'violet', 'brown', 'purple', 'black', 'white'],
+                delay: 100
+            },
+            showAutocompleteOnFocus: true
+        });
 
         $scope.stopWords = [
             "list of",
@@ -13,14 +22,15 @@ angular.module('wikiDiverApp')
         ];
 
         $scope.query = "";
-        $scope.regex = /en\.wikipedia\.org\/wiki\//g;
+        var regex = /en\.wikipedia\.org\/wiki\/.+/g;
         $scope.depth = 2;
         $scope.qarr = [];
         $scope.res = [];
         $scope.notFound=[];
         $scope.stopped=[];
-        $scope.download=false;
-        
+        $scope.edges=[];
+        $scope.nodes=[];
+
 
         $scope.update = function () {
 
@@ -28,18 +38,30 @@ angular.module('wikiDiverApp')
             $scope.download=false;
             $scope.notFound=[];
             $scope.stopped=[];
+            $scope.edges=[];
             $scope.res = [];
 
             if ($scope.query !== "") {
-               
-               $scope.qarr = $scope.query.split("\n");
-               $scope.qarr.forEach(function (e, i) {
-                    console.log("input",JSON.stringify(e));
-                    var ret = getSons(e, 0, $scope.res);
 
-                    if(ret===null) console.log("error");
-                })
-                $scope.download=true;
+                var error = false;
+
+                $scope.qarr = $scope.query.split("\n");
+
+                //check for integrity
+                $scope.qarr.forEach(function (e, i) {
+                    if(!regex.test(e)) error = true;
+                });
+
+                if(!error) {
+                    $scope.qarr.forEach(function (e, i) {
+                        console.log("input", JSON.stringify(e));
+                        var ret = getSons(e, 0, $scope.res);
+
+                        if (ret === null) console.log("error");
+                    })
+                    $scope.download = true;
+                }
+                else $scope.alert=true;
             }
             else $scope.alert=true;
         }
@@ -53,14 +75,14 @@ angular.module('wikiDiverApp')
 
             if (ind == 0) {
                 name = rgx.exec(line)[1];
+                $scope.nodes.push(decodeURIComponent(name).replace(/_/g, " "));
             }
 
             else name = encodeURIComponent(line.name);
 
-
             $http.jsonp('http://en.wikipedia.org/w/api.php?action=parse&page=' + name + '&prop=sections&format=json' + '&callback=JSON_CALLBACK').success(function (data) {
-                
-                if(data.parse===null || !data.parse) return null
+
+                if(data.parse===null || !data.parse) return null;
 
                 var p = data.parse;
                 index = null;
@@ -76,9 +98,9 @@ angular.module('wikiDiverApp')
                 if (index !== null) {
                     $http.jsonp('http://en.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&titles='+ name +'&rvprop=content&rvsection='+ index +'&callback=JSON_CALLBACK').success(function (links) {
 
-                    	var output = parseSection(links)
+                        var output = parseSection(links)
 
-                       output.forEach(function (d, j) {
+                        output.forEach(function (d, j) {
                             var found = false;
                             $scope.stopWords.forEach(function(a,b){
                                 if(d.toLowerCase().indexOf(a)>=0) {
@@ -89,6 +111,8 @@ angular.module('wikiDiverApp')
 
 
                             if(!found) {
+                                if($scope.nodes.indexOf(d)==-1)  $scope.nodes.push(d);
+                                $scope.edges.push({source:decodeURIComponent(name).replace(/_/g, " "),target:d,index:ind+1});
                                 sons.push({name: d,index:ind+1});
                             }
                         })
@@ -106,71 +130,47 @@ angular.module('wikiDiverApp')
                         }
 
                         if (ind + 1 < $scope.depth) {
-
                             sons.forEach(function (m, y) {
                                 m.sons = []
                                 getSons(m, ind + 1, m);
                             })
                         }
-
-
                     });
                 }
                 else {
-                    console.log("not found: ",name);
                     $scope.notFound.push(decodeURIComponent(name));
-                    //$http.jsonp('http://en.wikipedia.org/w/api.php?action=parse&page=' + name + '&prop=links&format=json' + '&callback=JSON_CALLBACK').success(function (found) {
-                    //    
-                    //});
                 }
-
             });
-    }
+        }
 
-    var parseSection=function(obj) {
-    	var o = obj.query.pages[Object.keys(obj.query.pages)[0]].revisions[0]['*'];
-    	var regex = /\[\[(.*?)\]\]/g;
+        var parseSection=function(obj) {
+            var o = obj.query.pages[Object.keys(obj.query.pages)[0]].revisions[0]['*'];
+            var regex = /\[\[(.*?)\]\]/g;
 
-		var matches, output = [];
-		while (matches = regex.exec(o)) {
-		    output.push(matches[1].split("|")[0]);
-		}
-		return output;
+            var matches, output = [];
+            while (matches = regex.exec(o)) {
+                output.push(matches[1].split("|")[0]);
+            }
+            return output;
 
-    }
+        }
 
 
-    $scope.downloadJSON = function() {
-         var json = angular.toJson($scope.res);
-        var blob = new Blob([json], { type: "data:text/json;charset=utf-8" });
-        saveAs(blob, "data.json")
-};
+        $scope.downloadJSON = function() {
+            var json = angular.toJson({nodes:$scope.nodes,edges:$scope.edges});
+            var blob = new Blob([json], { type: "data:text/json;charset=utf-8" });
+            saveAs(blob, "data.json")
+        };
 
- $scope.downloadCSV = function() {
+        $scope.downloadCSV = function() {
 
-        var csvtxt = ["source\ttarget\tdepth"];
-        csvtxt = appendLines(csvtxt,$scope.res);
-        var blob = new Blob([csvtxt.join("\n")], { type: "data:text/csv;charset=utf-8" });
-        saveAs(blob, "data.tsv")
-       	
-
-		function appendLines(txt,arr){
-
-			
-			arr.forEach(function(e,i){
-				var name = e.name;
-				
-				e.sons.forEach(function(f,j) {
-					var str=name+"\t"+f.name+"\t"+f.index;
-					if(txt.indexOf(str)==-1) txt.push(str);
-					if(f.sons && f.sons.length) {	
-						txt = appendLines(txt,e.sons);
-					}	
-				})
-			})
-			return txt;
-		}
-	};
-});
+            var csvtxt = "source\ttarget\tdepth\n";
+            $scope.edges.forEach(function(e,i){
+                csvtxt+=(e.source+"\t"+ e.target+"\t"+e.index+"\n");
+            })
+            var blob = new Blob([csvtxt], { type: "data:text/csv;charset=utf-8" });
+            saveAs(blob, "data.tsv")
+        };
+    });
 
 
