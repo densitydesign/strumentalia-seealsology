603e38ad215980acdb7d3b219ec958c721560746
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2025-01-13 10:36:13 +0100

    Updates

--- scripts/seeAlsology.633c2397.js
+++ scripts/seeAlsology.633c2397.js
@@ -132,6 +132,14 @@ angular.module('wikiDiverApp')
               'Portal:',
               'Fil:'
             ]
+          },
+          ca: {
+            name: 'catalan',
+            seeAlso: ['Vegeu també'],
+            stopWords: [
+              'Ajuda:',
+              'Annex:',
+            ]
           }
         };
         $scope.supportedLanguages = Object.keys(languages).map(function(l){ return languages[l].name; }).sort().join(', ');
